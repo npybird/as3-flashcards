9b721771a289df9f640eca4105e0ff4b2e0170ad
npybird <firstbird14120@gmail.com>
2026-04-13 01:13:00 +0700

    Initial Commit

---
+++ FlashCard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlashCard
+//
+//  Created by Natchanon Posayaanuwat on 13/4/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlashCard/FlashCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardApp.swift
+//  FlashCard
+//
+//  Created by Natchanon Posayaanuwat on 13/4/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
